33ae20b1c32011e20b43716ab6992500a15f1bed
Harshad Kasture <harshad@mit.edu>
2008-08-16 15:04:14 -0400

    Christopher Celio:   1. Made the test scripts into tcsh scripts.   2. Added a pthreads_basic test (ie, ping pong minus the CAPI calls).   3. Because of run-time undefined symbol bug (because pintools are now dlls?), took out inline keywords in core.cc.

--- common/core/core.cc
+++ common/core/core.cc
@@ -113,39 +113,41 @@ VOID Core::fini(int code, VOID *v, ofstream& out)
 
 //performance model wrappers
 
-inline VOID Core::perfModelRun(PerfModelIntervalStat *interval_stats)
+//BUG the following ten functions *should* be inlined, but 
+//we end up with undefined symbols at run-time. -cpc
+VOID Core::perfModelRun(PerfModelIntervalStat *interval_stats)
 { perf_model->run(interval_stats); }
 
-inline VOID Core::perfModelRun(PerfModelIntervalStat *interval_stats, REG *reads, 
+VOID Core::perfModelRun(PerfModelIntervalStat *interval_stats, REG *reads, 
 						 UINT32 num_reads)
 { perf_model->run(interval_stats, reads, num_reads); }
 
-inline VOID Core::perfModelRun(PerfModelIntervalStat *interval_stats, bool dcache_load_hit, 
+VOID Core::perfModelRun(PerfModelIntervalStat *interval_stats, bool dcache_load_hit, 
 						 REG *writes, UINT32 num_writes)
 { perf_model->run(interval_stats, dcache_load_hit, writes, num_writes); }
 
-inline PerfModelIntervalStat* Core::perfModelAnalyzeInterval(const string& parent_routine, 
+PerfModelIntervalStat* Core::perfModelAnalyzeInterval(const string& parent_routine, 
 													   const INS& start_ins, 
 													   const INS& end_ins)
 { return perf_model->analyzeInterval(parent_routine, start_ins, end_ins); }
 
-inline VOID Core::perfModelLogICacheLoadAccess(PerfModelIntervalStat *stats, bool hit)
+VOID Core::perfModelLogICacheLoadAccess(PerfModelIntervalStat *stats, bool hit)
 { perf_model->logICacheLoadAccess(stats, hit); }
 
-inline VOID Core::perfModelLogDCacheStoreAccess(PerfModelIntervalStat *stats, bool hit)
+VOID Core::perfModelLogDCacheStoreAccess(PerfModelIntervalStat *stats, bool hit)
 { perf_model->logDCacheStoreAccess(stats, hit); }
 
-inline VOID Core::perfModelLogBranchPrediction(PerfModelIntervalStat *stats, bool correct)
+VOID Core::perfModelLogBranchPrediction(PerfModelIntervalStat *stats, bool correct)
 { perf_model->logBranchPrediction(stats, correct); }
 
 
 // organic cache wrappers
 
-inline bool Core::icacheRunLoadModel(ADDRINT i_addr, UINT32 size)
+bool Core::icacheRunLoadModel(ADDRINT i_addr, UINT32 size)
 { return ocache->runICacheLoadModel(i_addr, size); }
 
-inline bool Core::dcacheRunLoadModel(ADDRINT d_addr, UINT32 size)
+bool Core::dcacheRunLoadModel(ADDRINT d_addr, UINT32 size)
 { return memory_manager->runDCacheLoadModel(d_addr, size); }
 
-inline bool Core::dcacheRunStoreModel(ADDRINT d_addr, UINT32 size)
+bool Core::dcacheRunStoreModel(ADDRINT d_addr, UINT32 size)
 { return memory_manager->runDCacheStoreModel(d_addr, size); }

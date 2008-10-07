7d8b8662f06750dd75bbd92096ac2f39881adf40
Charles Gruenwald <cg3@mit.edu>
2008-10-07 13:44:17 -0400

    adding an example for skipping over system calls

---
+++ common/tests/skip_system_call/pin_sim.cpp
@@ -0,0 +1,59 @@
+
+#include <iostream>
+#include <syscall.h>
+#include "pin.H"
+
+bool called_open = false;
+
+using namespace std;
+
+void SyscallEntry(THREADID threadIndex, CONTEXT *ctxt, SYSCALL_STANDARD std, VOID *v)
+{
+
+   int syscall_number = PIN_GetSyscallNumber(ctxt, std);
+
+   switch(syscall_number)
+   {
+      case SYS_open:
+      {
+         char *path = (char *)PIN_GetSyscallArgument(ctxt, std, 0);
+         if(!strcmp(path,"./input"))
+         {
+            called_open = true;
+            cout << "open(" << path << ")" << endl;
+
+            int return_addr = PIN_GetContextReg(ctxt, REG_INST_PTR);
+            return_addr += 2;
+            PIN_SetContextReg(ctxt, REG_INST_PTR, return_addr);
+            PIN_SetContextReg(ctxt, REG_EAX, 0x8);
+
+            PIN_ExecuteAt(ctxt);
+         }
+
+         break;
+      }
+      default:
+         break;
+   }
+}
+
+void SyscallExit(THREADID threadIndex, CONTEXT *ctxt, SYSCALL_STANDARD std, VOID *v)
+{
+    PIN_SetContextReg(ctxt, REG_EAX, 0x7);
+}
+
+
+int main(int argc, char *argv[])
+{
+    PIN_Init(argc, argv);
+
+    PIN_AddSyscallEntryFunction(SyscallEntry, 0);
+    PIN_AddSyscallExitFunction(SyscallExit, 0);
+
+
+    // Never returns
+    PIN_StartProgram();
+    
+    return 0;
+}
+

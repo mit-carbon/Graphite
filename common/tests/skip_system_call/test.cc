41b0abde86f6ee28b69fb0553dd8818d59983b8e
Jonathan Eastep <eastepjm@cagdesk3.csail.mit.edu>
2008-10-07 14:16:26 -0400

    Fixing Charle's stupid mistake :)

---
+++ common/tests/skip_system_call/test.cc
@@ -0,0 +1,14 @@
+
+#include <iostream>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+
+using namespace std;
+
+int main(int argc, char **argv)
+{
+    int i = open("./input", O_RDONLY);
+    cout << "fid: " << i << endl;
+
+}

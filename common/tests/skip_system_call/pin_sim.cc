f8d0e36de4666cc82ca10492931622a74ed54326
Jonathan Eastep <eastepjm@cagdesk3.csail.mit.edu>
2008-10-07 16:47:40 -0400

    Now we change syscall number so an innocuous syscall is executed

--- common/tests/skip_system_call/pin_sim.cc
+++ common/tests/skip_system_call/pin_sim.cc
@@ -21,12 +21,17 @@ void SyscallEntry(THREADID threadIndex, CONTEXT *ctxt, SYSCALL_STANDARD std, VOI
             called_open = true;
             cout << "open(" << path << ")" << endl;
 
+	    // safer than letting the original syscall go
+	    PIN_SetSyscallNumber(ctxt, std, SYS_getpid);
+
+#if 0
             int return_addr = PIN_GetContextReg(ctxt, REG_INST_PTR);
             return_addr += 2;
             PIN_SetContextReg(ctxt, REG_INST_PTR, return_addr);
             PIN_SetContextReg(ctxt, REG_EAX, 0x8);
 
             PIN_ExecuteAt(ctxt);
+#endif
          }
 
          break;

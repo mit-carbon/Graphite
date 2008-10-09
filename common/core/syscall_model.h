92fc66e894a9ff9c9b6a519f842e86ba37f2cbd0
Jonathan Eastep <eastepjm@cagdesk1.csail.mit.edu>
2008-10-09 13:42:26 -0400

    file_io test. started writing code for using pt layer MCP stuff. isn't working yet, but does not affect anything but io_test. currently can't build io_test because of undefined references.

--- common/core/syscall_model.h
+++ common/core/syscall_model.h
@@ -2,15 +2,20 @@
 #define SYSCALL_MODEL_H
 
 #include <iostream>
+#include "transport.h"
 #include "pin.H"
 
 class SyscallMdl
 {
+   private:
+      void marshallOpenCall(CONTEXT *ctx, SYSCALL_STANDARD syscall_standard);
+
    public:
       SyscallMdl();
       void runEnter(int rank, CONTEXT *ctx, SYSCALL_STANDARD syscall_standard);
       void runExit(int rank, CONTEXT *ctx, SYSCALL_STANDARD syscall_standard);
-   private:
+
+
 };
 
 #endif

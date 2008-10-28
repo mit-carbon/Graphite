cc2f687b11d9592f77cd155382d9d0c347e6875a
Charles Gruenwald <cg3@cagnode16.csail.mit.edu>
2008-10-28 17:35:08 -0400

    [misc] Stable iterator fixes.

--- common/misc/stable_iterator.h
+++ common/misc/stable_iterator.h
@@ -7,11 +7,19 @@ template
 <class T> class StableIterator
 {
   public:
+    StableIterator(const StableIterator<T> &s)
+        : _vec(s._vec), _offset(s._offset) {}
+
     StableIterator(std::vector<T> &vec, unsigned int offset)
         : _vec(vec), _offset(offset) {}
     T* getPtr() { return &(_vec[_offset]); }
     T* operator->(){ return getPtr(); }
     T& operator*(){ return *getPtr(); }
+
+    StableIterator<T> operator=(const StableIterator<T>&src)
+    {
+        return StableIterator<T>(src._vec, src._offset);
+    }
   private:
     std::vector<T> & _vec;
     unsigned int _offset;

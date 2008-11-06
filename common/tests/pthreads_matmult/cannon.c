c40bee79b3ed8ba55eb94d36f1637b9816491195
Jason E Miller <jasonm@mit.edu>
2008-11-06 17:34:21 -0500

    Convert cannon to a pure C program for compatibility with newlib

--- common/tests/pthreads_matmult/cannon.c
+++ common/tests/pthreads_matmult/cannon.c
@@ -10,14 +10,15 @@
  *                          01/06/2008
  ****************************************************/
 
-#include <iostream>
 #include <pthread.h>
 #include <math.h>
 #include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
 #include "capi.h"
 
-using namespace std;
 
+//#define DEBUG 1
 
 // Declare global variables
 float **a, **b, **c;
@@ -42,11 +43,17 @@ int main(int argc, char* argv[]){ // main begins
 	// Read in the command line arguments
 	
 	unsigned int matSize, numThreads;
+	pthread_t* threads;
+	pthread_attr_t attr;
+
+#ifdef DEBUG
+	printf("This is the function main()\n");
+#endif
 
 	if(argc != 5){
-		cout << "Invalid command line options. The correct format is:" << endl;
-		cout << "cannon -m num_of_threads -s size_of_square_matrix" << endl;
-		exit(EXIT_FAILURE);
+	   printf("Invalid command line options. The correct format is:\n"); 
+	   printf("cannon -m num_of_threads -s size_of_square_matrix\n");
+	   exit(EXIT_FAILURE);
 	}
 	else{
 		if((strcmp(argv[1], "-m\0") == 0) && (strcmp(argv[3], "-s\0") == 0)){
@@ -58,49 +65,52 @@ int main(int argc, char* argv[]){ // main begins
 			matSize = atoi(argv[2]);
 		}
 		else{
-			cout << "Invalid command line options. The correct format is:" << endl;
-			cout << "cannon -m num_of_threads -s size_of_square_matrix" << endl;
-			exit(EXIT_FAILURE);
+		   printf("Invalid command line options. The correct format is:\n"); 
+		   printf("cannon -m num_of_threads -s size_of_square_matrix\n");
+		   exit(EXIT_FAILURE);
 		}
 	}
 
+#ifdef DEBUG
+	printf("Number of threads: %d     Matrix size: %d\n", numThreads, matSize);
+#endif
+
 	// Declare threads and related variables
-	pthread_t threads[numThreads];
-	pthread_attr_t attr;
+	threads = (pthread_t*)malloc(numThreads*sizeof(pthread_t));
 	
 #ifdef DEBUG
-	cout << "This is the function main()" << endl;
+	printf("Initializing global variables\n");
 #endif
 	// Initialize global variables
 
 	// Debug
 	numSendCalls = 0;
-	numThreadSends = new long [numThreads];
+	numThreadSends = (long*)malloc(numThreads*sizeof(long));
 	for(unsigned int i = 0; i < numThreads; i++) numThreadSends[i] = 0;
 
 	// Debug
 	numReceiveCalls = 0;
-	numThreadReceives = new long [numThreads];
+	numThreadReceives = (long*)malloc(numThreads*sizeof(long));
 	for(unsigned int i = 0; i < numThreads; i++) numThreadReceives[i] = 0;
 
 	// Initialize a
-	a = (float**) new float*[matSize];
+	a = (float**)malloc(matSize*sizeof(float*));
 	for(unsigned int i = 0; i < matSize; i++){
-		a[i] = (float*) new float[matSize];
+	   a[i] = (float*)malloc(matSize*sizeof(float));
 		for(unsigned int j = 0; j < matSize; j++) a[i][j] = 2;
 	}
 
 	// Initialize b
-	b = (float**) new float*[matSize];
+	b = (float**)malloc(matSize*sizeof(float*));
 	for(unsigned int i = 0; i < matSize; i++){
-		b[i] = (float*) new float[matSize];
+	   b[i] = (float*)malloc(matSize*sizeof(float));
 		for(unsigned int j = 0; j < matSize; j++) b[i][j] = 3;
 	}
 
 	// Initialize c
-	c = (float**) new float*[matSize];
+	c = (float**)malloc(matSize*sizeof(float*));
 	for(unsigned int i = 0; i < matSize; i++){
-		c[i] = (float*) new float[matSize];
+	   c[i] = (float*)malloc(matSize*sizeof(float));
 		for(unsigned int j = 0; j < matSize; j++) c[i][j] = 0;
 	}
 	
@@ -113,7 +123,7 @@ int main(int argc, char* argv[]){ // main begins
 	blockSize = matSize / sqrtNumProcs;
 
 #ifdef DEBUG
-	cout << "Initializing thread structures" << endl;
+	printf("Initializing thread structures\n");
 	pthread_mutex_init(&lock, NULL);
 #endif
 
@@ -122,25 +132,34 @@ int main(int argc, char* argv[]){ // main begins
 	pthread_attr_setdetachstate(&attr, PTHREAD_CREATE_JOINABLE);
 	pthread_mutex_init(&write_lock, NULL);
 #ifdef DEBUG
-	cout << "Spawning threads" << endl;
+	printf("Spawning threads\n");
 #endif
-	for(unsigned int i = 0; i < numThreads; i++) pthread_create(&threads[i], &attr, cannon, (void *) i);
+	for(unsigned int i = 0; i < numThreads; i++) {
+	   int err = pthread_create(&threads[i], &attr, cannon, (void *) i);
+	   if (err != 0) {
+	      printf("  ERROR spawning thread %d!  Error code: %s\n",
+		     i, strerror(err));
+	   }
+#ifdef DEBUG
+	   else { printf("  Spawned thread %d\n", i); }
+#endif
+	}
 
 	// Wait for all threads to complete
 	for(unsigned int i = 0; i < numThreads; i++) pthread_join(threads[i], NULL);
 
 	// Print out the result matrix
-	cout << "c = " << endl;
+	printf("c = \n");
 	for(unsigned int i = 0; i < matSize; i++){
 		for(unsigned int j = 0; j < matSize; j++){
-			cout << c[i][j] << " ";
+		   printf("%f ", c[i][j]);
 		}
-		cout << endl;
+		printf("\n");
 	}
 
 	// Debug
-	cout << "Number of sends = " << numSendCalls << endl;
-	cout << "Number of receives = " << numReceiveCalls << endl;
+	printf("Number of sends = %ld\n", numSendCalls);
+	printf("Number of receives = %ld\n", numReceiveCalls);
 
 	pthread_exit(NULL);
 } // main ends
@@ -154,6 +173,10 @@ void* cannon(void *threadid){
 	int i, j;
 	int ai, aj, bi, bj;
 	CAPI_return_t rtnVal;
+
+#ifdef DEBUG
+	printf("Starting thread %d\n", (unsigned int)threadid);
+#endif
 
 	rtnVal = CAPI_Initialize(&tid);
 
@@ -186,23 +209,23 @@ void* cannon(void *threadid){
 	bj = j * blockSize;
 	
 	// populate aBlock
-	aBlock = (float**)new float*[blockSize];
+	aBlock = (float**)malloc(blockSize*sizeof(float*));
 	for(int x = 0; x < blockSize; x++){
-		aBlock[x] = (float*)new float[blockSize];
+		aBlock[x] = (float*)malloc(blockSize*sizeof(float));
 		for(int y = 0; y < blockSize; y++) aBlock[x][y] = a[ai + x][aj + y];
 	}
 
 	// populate bBlock
-	bBlock = (float**)new float*[blockSize];
+	bBlock = (float**)malloc(blockSize*sizeof(float*));
 	for(int x = 0; x < blockSize; x++){
-		bBlock[x] = (float*)new float[blockSize];
+		bBlock[x] = (float*)malloc(blockSize*sizeof(float));
 		for(int y = 0; y < blockSize; y++) bBlock[x][y] = b[bi + x][bj + y];
 	}
 
 	// Initialize cBlock
-	cBlock = (float**)new float*[blockSize];
+	cBlock = (float**)malloc(blockSize*sizeof(float*));
 	for(int x = 0; x < blockSize; x++){
-		cBlock[x] = (float*)new float[blockSize];
+		cBlock[x] = (float*)malloc(blockSize*sizeof(float));
 		for(int y = 0; y < blockSize; y++) cBlock[x][y] = 0;
 	}
 
@@ -218,7 +241,7 @@ void* cannon(void *threadid){
 				for(int y = 0; y < blockSize; y++) {
 #ifdef DEBUG
 					pthread_mutex_lock(&lock);
-					cout << "tid #" << tid << " sending to tid #" << leftProc << endl;
+					printf("tid # %d sending to tid # %d\n", tid, leftProc);
 					pthread_mutex_unlock(&lock);
 #endif
 					CAPI_message_send_w((CAPI_endpoint_t)tid, (CAPI_endpoint_t)leftProc, (char*)&aBlock[x][y], sizeof(float));
@@ -234,7 +257,7 @@ void* cannon(void *threadid){
 				for(int y = 0; y < blockSize; y++) {
 #ifdef DEBUG
 					pthread_mutex_lock(&lock);
-					cout << "tid #" << tid << " sending to tid #" << upProc << endl;
+					printf("tid # %d sending to tid # %d\n", tid, upProc);
 					pthread_mutex_unlock(&lock);
 #endif
 					CAPI_message_send_w((CAPI_endpoint_t)tid, (CAPI_endpoint_t)upProc, (char*)&bBlock[x][y], sizeof(float));
@@ -251,7 +274,7 @@ void* cannon(void *threadid){
 				for(int y = 0; y < blockSize; y++) {
 #ifdef DEBUG
 					pthread_mutex_lock(&lock);
-					cout << "tid #" << tid << " receiving from tid #" << rightProc << endl;
+					printf("tid # %d receiving from tid # %d\n", tid, rightProc);
 					pthread_mutex_unlock(&lock);
 #endif
 					CAPI_message_receive_w((CAPI_endpoint_t)rightProc, (CAPI_endpoint_t)tid, (char*)&aBlock[x][y], sizeof(float));
@@ -267,7 +290,7 @@ void* cannon(void *threadid){
 				for(int y = 0; y < blockSize; y++) {
 #ifdef DEBUG
 					pthread_mutex_lock(&lock);
-					cout << "tid #" << tid << " receiving from tid #" << downProc << endl;
+					printf("tid # %d receiving from tid # %d\n", tid, downProc);
 					pthread_mutex_unlock(&lock);
 #endif
 					CAPI_message_receive_w((CAPI_endpoint_t)downProc, (CAPI_endpoint_t)tid, (char*)&bBlock[x][y], sizeof(float));

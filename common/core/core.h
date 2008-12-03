f860d2b74545a1b58ebf701e7964c6ca77f0a0c9
Jonathan Eastep <eastepjm@cagnode18.csail.mit.edu>
2008-12-03 00:31:34 -0500

    performance modeling. fixed a bug that was counting cpu cycles wrong. microops and thus cycles were sometimes being counted more than once. fix is code-complete. still testing correctness.

--- common/core/core.h
+++ common/core/core.h
@@ -50,7 +50,7 @@ class Core
    private:
       int core_tid;
       int core_num_mod;
-		MemoryManager *memory_manager;
+      MemoryManager *memory_manager;
       Network *network;
       PerfModel *perf_model;
       OCache *ocache;
@@ -61,10 +61,11 @@ class Core
 
    public:
 
-		enum mem_operation_t {
-			LOAD,
-			STORE
-		};
+      enum mem_operation_t 
+      {
+         LOAD,
+         STORE
+      };
 
       int getRank() 
          { return core_tid; }
@@ -74,7 +75,7 @@ class Core
          { return core_num_mod; }
 
       
-		int coreInit(int tid, int num_mod);
+      int coreInit(int tid, int num_mod);
 
       // Return the communication endpoint ID for this core
       int coreCommID();
@@ -92,46 +93,46 @@ class Core
       
       VOID fini(int code, VOID *v, ofstream& out);
 	
-		// organic cache wrappers
+      // organic cache wrappers
 	
-		bool icacheRunLoadModel(ADDRINT i_addr, UINT32 size);
+      bool icacheRunLoadModel(ADDRINT i_addr, UINT32 size);
 	
-		bool dcacheRunModel(mem_operation_t operation, ADDRINT d_addr, char* data_buffer, UINT32 data_size);
+      bool dcacheRunModel(mem_operation_t operation, ADDRINT d_addr, char* data_buffer, UINT32 data_size);
 
-		// FIXME: Debug Functions. Debug hook to smash cache state
-		void debugSetCacheState(ADDRINT addr, CacheState::cstate_t cstate, char *c_data);
-		//return true if assertion is true  (that's a good thing)
-		bool debugAssertCacheState(ADDRINT addr, CacheState::cstate_t cstate, char *c_data);
+      // FIXME: Debug Functions. Debug hook to smash cache state
+      void debugSetCacheState(ADDRINT addr, CacheState::cstate_t cstate, char *c_data);
+      //return true if assertion is true  (that's a good thing)
+      bool debugAssertCacheState(ADDRINT addr, CacheState::cstate_t cstate, char *c_data);
 
-		void debugSetDramState(ADDRINT addr, DramDirectoryEntry::dstate_t dstate, vector<UINT32> sharers_list, char *d_data);
-		bool debugAssertDramState(ADDRINT addr, DramDirectoryEntry::dstate_t dstate, vector<UINT32> sharers_list, char *d_data);
+      void debugSetDramState(ADDRINT addr, DramDirectoryEntry::dstate_t dstate, vector<UINT32> sharers_list, char *d_data);
+      bool debugAssertDramState(ADDRINT addr, DramDirectoryEntry::dstate_t dstate, vector<UINT32> sharers_list, char *d_data);
 	
-		//performance model wrappers
+      //performance model wrappers
 
-		VOID perfModelRun(PerfModelIntervalStat *interval_stats)
-		{ perf_model->run(interval_stats); }
+      VOID perfModelRun(PerfModelIntervalStat *interval_stats, bool firstCallInIntrvl)
+         { perf_model->run(interval_stats, firstCallInIntrvl); }
 
-		VOID perfModelRun(PerfModelIntervalStat *interval_stats, REG *reads, 
-									 UINT32 num_reads)
-		{ perf_model->run(interval_stats, reads, num_reads); }
+      VOID perfModelRun(PerfModelIntervalStat *interval_stats, REG *reads, 
+                        UINT32 num_reads, bool firstCallInIntrvl)
+         { perf_model->run(interval_stats, reads, num_reads, firstCallInIntrvl); }
 
-		VOID perfModelRun(PerfModelIntervalStat *interval_stats, bool dcache_load_hit, 
-									 REG *writes, UINT32 num_writes)
-		{ perf_model->run(interval_stats, dcache_load_hit, writes, num_writes); }
+      VOID perfModelRun(PerfModelIntervalStat *interval_stats, bool dcache_load_hit, 
+                        REG *writes, UINT32 num_writes, bool firstCallInIntrvl)
+         { perf_model->run(interval_stats, dcache_load_hit, writes, num_writes, firstCallInIntrvl); }
 
-		PerfModelIntervalStat* perfModelAnalyzeInterval(const string& parent_routine, 
-																			 const INS& start_ins, 
-																			 const INS& end_ins)
-		{ return perf_model->analyzeInterval(parent_routine, start_ins, end_ins); }
+      PerfModelIntervalStat* perfModelAnalyzeInterval(const string& parent_routine,
+                                                      const INS& start_ins,
+                                                      const INS& end_ins)
+         { return perf_model->analyzeInterval(parent_routine, start_ins, end_ins); }
 
-		VOID perfModelLogICacheLoadAccess(PerfModelIntervalStat *stats, bool hit)
-		{ perf_model->logICacheLoadAccess(stats, hit); }
+      VOID perfModelLogICacheLoadAccess(PerfModelIntervalStat *stats, bool hit)
+         { perf_model->logICacheLoadAccess(stats, hit); }
 
-		VOID perfModelLogDCacheStoreAccess(PerfModelIntervalStat *stats, bool hit)
-		{ perf_model->logDCacheStoreAccess(stats, hit); }
+      VOID perfModelLogDCacheStoreAccess(PerfModelIntervalStat *stats, bool hit)
+         { perf_model->logDCacheStoreAccess(stats, hit); }
 
-		VOID perfModelLogBranchPrediction(PerfModelIntervalStat *stats, bool correct)
-		{ perf_model->logBranchPrediction(stats, correct); }
+      VOID perfModelLogBranchPrediction(PerfModelIntervalStat *stats, bool correct)
+         { perf_model->logBranchPrediction(stats, correct); }
 	
 
       // add proc time management to core

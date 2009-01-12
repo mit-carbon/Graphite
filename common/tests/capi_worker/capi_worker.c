723d45b05a4899ed21172d27dae80f41743abbb0
Nathan Beckmann <beckmann@mit.edu>
2009-01-12 18:03:47 -0500

    [tests/capi_worker] Charles's broken test case (now works).

--- common/tests/capi_worker/capi_worker.c
+++ common/tests/capi_worker/capi_worker.c
@@ -1,10 +1,7 @@
-/********************************************************
- * This test will send arrays values to a sub-thread to *
- * be multiplied.                                       *
- *                          - Charles Gruenwald         *
- *                          12/12/2008                  *
- *******************************************************/
-
+/******************************************************************
+ * This test sends arrays values to a sub-thread to be multiplied.*
+ *                      - Charles Gruenwald  (12/12/2008)         *
+ *****************************************************************/
 #include <assert.h>
 #include <pthread.h>
 #include <math.h>
@@ -13,108 +10,64 @@
 #include <string.h>
 #include "user_api.h"
 
-unsigned int numThreads = 8;
+#define SIZE 8
 
-#define SIZE 1024
-
-// Function executed by each thread
 void* worker(void * threadid);
 
+unsigned int numThreads = 2;
+
 int main(int argc, char* argv[]){ // main begins
+   pthread_t* threads;
+   pthread_attr_t attr;
+   float m[SIZE];
+   int c = 3;
+
+   for(unsigned int i = 0; i < SIZE; i++)
+      m[i] = (float)i;
 
    carbonInit();
-	
-	
-	pthread_t* threads;
-	pthread_attr_t attr;
+   CAPI_Initialize((unsigned int)numThreads);
 
-	// Declare threads and related variables
-	threads = (pthread_t*)malloc(numThreads*sizeof(pthread_t));
-	
-	CAPI_return_t rtnVal;
-	rtnVal = CAPI_Initialize((unsigned int)numThreads);
+   threads = (pthread_t*)malloc(numThreads*sizeof(pthread_t));
 
-	// Initialize threads and related variables
-	pthread_attr_init(&attr);
-	pthread_attr_setdetachstate(&attr, PTHREAD_CREATE_JOINABLE);
+   // Initialize threads and related variables
+   pthread_attr_init(&attr);
+   pthread_attr_setdetachstate(&attr, PTHREAD_CREATE_JOINABLE);
 
-   float *a = (float*)malloc(SIZE*sizeof(float));
-   float *b = (float*)malloc(SIZE*sizeof(float));
-   float *c = (float*)malloc(SIZE*sizeof(float));
-
-   for(unsigned int i = 0; i < SIZE; i++)
-      a[i] = (float)i;
-
-   for(unsigned int i = 0; i < SIZE; i++)
-      b[i] = (float)2*i;
-
-	for(unsigned int i = 0; i < numThreads; i++) {
+   for(unsigned int i = 0; i < numThreads; i++) {
       int tid = i;
 
       printf("Spawning worker thread: %d\n", i);
-	   int err = pthread_create(&threads[i], &attr, worker, (void *) i);
-	   if (err != 0) {
-	      printf("  ERROR spawning thread %d!  Error code: %s\n", i, strerror(err));
-	   }
-	   else { 
-          CAPI_message_send_w((CAPI_endpoint_t)numThreads, (CAPI_endpoint_t)tid, (char *)a, sizeof(float) * SIZE);
-          CAPI_message_send_w((CAPI_endpoint_t)numThreads, (CAPI_endpoint_t)tid, (char *)b, sizeof(float) * SIZE);
-      }
-	}
+      pthread_create(&threads[i], &attr, worker, (void *) i);
+      bool started;
+      CAPI_message_receive_w((CAPI_endpoint_t)tid, (CAPI_endpoint_t)numThreads, (char *)&started, sizeof(started));
 
-	// Wait for all threads to complete
-	for(unsigned int i = 0; i < numThreads; i++) 
-   {
-      int tid = i;
-      printf("Waiting for %d\n", tid);
-      CAPI_message_receive_w((CAPI_endpoint_t)tid, (CAPI_endpoint_t)numThreads, (char *)c, sizeof(float) * SIZE);
-      for(unsigned int j = 0; j < SIZE; j++)
-      {
-         if(c[i] != a[i] * b[i])
-            printf("Error in multiplication: %f should be %f * %f = %f\n", c[i], a[i], b[i], a[i] * b[i]);
-         assert(c[i] == a[i] * b[i]); 
-      }
-      pthread_join(threads[i], NULL);
+      CAPI_message_send_w((CAPI_endpoint_t)numThreads, (CAPI_endpoint_t)tid, (char *)&c, sizeof(c));
+      CAPI_message_send_w((CAPI_endpoint_t)numThreads, (CAPI_endpoint_t)tid, (char *)m, sizeof(float) * SIZE);
    }
 
+   printf("Done... joining.\n");
+   for(unsigned int i = 0; i < numThreads; i++) 
+      pthread_join(threads[i], NULL);
 
    free(threads);
-   free(a);
-   free(b);
-   free(c);
-
    carbonFinish();
-
-	pthread_exit(NULL);
+   pthread_exit(NULL);
+   printf("Done\n");
 } // main ends
 
 void* worker(void *threadid){
+   int c;
+   float m[SIZE];
+   bool started = true;
 
-	// Declare local variables
-	int tid;
+   CAPI_Initialize((unsigned int)threadid);
 
-	CAPI_return_t rtnVal;
-	rtnVal = CAPI_Initialize((unsigned int)threadid);
-	CAPI_rank(&tid);
+   CAPI_message_send_w((CAPI_endpoint_t)threadid, (CAPI_endpoint_t)numThreads, (char *)&started, sizeof(started));
 
-   printf("Worker thread got id: %d\n", tid);
+   CAPI_message_receive_w((CAPI_endpoint_t)numThreads, (CAPI_endpoint_t)threadid, (char *)&c, sizeof(c));
+   CAPI_message_receive_w((CAPI_endpoint_t)numThreads, (CAPI_endpoint_t)threadid, (char *)m, sizeof(float) * SIZE);
 
-   float *a = (float*)malloc(SIZE*sizeof(float));
-   float *b = (float*)malloc(SIZE*sizeof(float));
-   float *c = (float*)malloc(SIZE*sizeof(float));
-   CAPI_message_receive_w((CAPI_endpoint_t)numThreads, (CAPI_endpoint_t)tid, (char *)a, sizeof(float) * SIZE);
-   CAPI_message_receive_w((CAPI_endpoint_t)numThreads, (CAPI_endpoint_t)tid, (char *)b, sizeof(float) * SIZE);
-
-   for(unsigned int i = 0; i < SIZE; i++)
-      c[i] = a[i] * b[i];
-
-   printf(" Sending c...\n");
-   CAPI_message_send_w((CAPI_endpoint_t)tid, (CAPI_endpoint_t)numThreads, (char *)c, sizeof(float) * SIZE);
-
-   free(a);
-   free(b);
-   free(c);
-
-   return NULL;
+   pthread_exit(NULL);
 }
 

30facd5390a9555634e81bae223cba64d2f821ff
Charles Gruenwald <cg3@cagnode16.csail.mit.edu>
2009-02-13 15:14:07 -0500

    [cleanup] Public at top.

--- common/system/network_model_analytical_server.h
+++ common/system/network_model_analytical_server.h
@@ -11,17 +11,16 @@ typedef UInt32 core_id_t;
 
 class NetworkModelAnalyticalServer
 {
-   private:
-      std::vector<double> _local_utilizations;
-
-      Network & _network;
-      UnstructuredBuffer & _recv_buffer;
-
    public:
       NetworkModelAnalyticalServer(Network &network, UnstructuredBuffer &recv_buffer);
       ~NetworkModelAnalyticalServer();
 
       void update(core_id_t);
-};
 
+   private:
+      std::vector<double> _local_utilizations;
+
+      Network & _network;
+      UnstructuredBuffer & _recv_buffer;
+};
 #endif

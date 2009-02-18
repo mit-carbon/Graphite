8ded18aa329614a10cc99d18c7dbda12070a455d
Charles Gruenwald <cg3@cagnode16.csail.mit.edu>
2009-02-18 11:58:00 -0500

    [transport] sm transport compiles again.

--- common/phys_trans/smtransport.cc
+++ common/phys_trans/smtransport.cc
@@ -5,8 +5,6 @@
 Transport::PTQueue* Transport::pt_queue = NULL;
 Transport::Futex* Transport::pt_futx = NULL;
 
-UInt32 Transport::s_pt_num_mod = 0;
-
 void Transport::ptInitQueue(SInt32 num_mod)
 {
    SInt32 i;
@@ -14,46 +12,20 @@ void Transport::ptInitQueue(SInt32 num_mod)
    pt_queue = new PTQueue[num_mod];
    pt_futx = new Futex[num_mod];
 
-   /*
-   mcp_queue = new MCPQueue[num_mod+1];
-   mcp_futx = new Futex[num_mod+1];
-   mcp_idx = (UInt32)num_mod;
-    */
-
-   s_pt_num_mod = (UInt32)num_mod;
-
    for (i = 0; i < num_mod; i++)
    {
       InitLock(&(pt_queue[i].pt_q_lock));
       pt_futx[i].futx = 1;
       InitLock(&(pt_futx[i].futx_lock));
 
-      // Initialize the queues that the MCP will use to send messages
-      //  back to the modules
-      /*
-      InitLock(&(mcp_queue[i].q_lock));
-      mcp_futx[i].futx = 1;
-      InitLock(&(mcp_futx[i].futx_lock));
-       */
    }
-
-   // Initialize the extra queue for the MCP
-   /*
-   InitLock(&(mcp_queue[mcp_idx].q_lock));
-   mcp_futx[mcp_idx].futx = 1;
-   InitLock(&(mcp_futx[mcp_idx].futx_lock));
-    */
 
 }
 
-SInt32 Transport::ptInit(SInt32 tid, SInt32 num_mod)
+Transport::Transport(SInt32 tid)
 {
    pt_tid = tid;
-   pt_num_mod = num_mod;
-   assert((UInt32)pt_tid < s_pt_num_mod);
-   assert((UInt32)pt_num_mod == s_pt_num_mod);
    InitLock(&(pt_futx[tid].futx_lock));
-   return 0;
 }
 
 SInt32 Transport::ptSend(SInt32 receiver, void *buffer, SInt32 size)
@@ -62,7 +34,6 @@ SInt32 Transport::ptSend(SInt32 receiver, void *buffer, SInt32 size)
    Byte *bufcopy = new Byte[size];
    memcpy(bufcopy, buffer, size);
 
-   assert(0 <= receiver && receiver < pt_num_mod);
    GetLock(&(pt_queue[receiver].pt_q_lock), 1);
    pt_queue[receiver].pt_queue.push(bufcopy);
    ReleaseLock(&(pt_queue[receiver].pt_q_lock));
@@ -84,7 +55,6 @@ SInt32 Transport::ptSend(SInt32 receiver, void *buffer, SInt32 size)
 void* Transport::ptRecv()
 {
    void *ptr;
-   assert(0 <= pt_tid && pt_tid < pt_num_mod);
 
    while (1)
    {
@@ -112,6 +82,5 @@ void* Transport::ptRecv()
 Boolean Transport::ptQuery()
 {
    //original code
-   assert(0 <= pt_tid && pt_tid < pt_num_mod);
    return !(pt_queue[pt_tid].pt_queue.empty());
 }
--- common/phys_trans/smtransport.h
+++ common/phys_trans/smtransport.h
@@ -18,9 +18,29 @@
 
 class Transport
 {
+   public:
+      Transport(SInt32 core_id);
+
+      // This routine should be called once within in each process.
+      static void ptGlobalInit();
+
+      // This does nothing but is needed so the interface matches the
+      //  other versions of the PT layer
+      static void ptFinish() {}
+
+      //FIXME: this should become a phtread barrier
+      static void ptBarrier() {}
+
+      SInt32 ptSend(SInt32 receiver, void *buffer, SInt32 length);
+
+      void* ptRecv();
+
+      Boolean ptQuery();
+
+      static void ptInitQueue(SInt32 num_mod);
+
    private:
       SInt32 pt_tid;
-      SInt32 pt_num_mod;
 
       //***** Data structures for normal communication *****//
       typedef struct PTQueue
@@ -39,24 +59,6 @@ class Transport
 
       static Futex *pt_futx;
 
-      //***** Miscellaneous *****//
-      static UInt32 s_pt_num_mod; // used for bounds checking only
-
-   public:
-      SInt32 ptInit(SInt32 tid, SInt32 num_mod);
-
-      // This does nothing but is needed so the interface matches the
-      //  other versions of the PT layer
-      static void ptFinish() {}
-
-      // CommID and ThreadID are the same in this version of the PT layer
-      SInt32 ptCommID() { return pt_tid; }
-
-
-      SInt32 ptSend(SInt32 receiver, void *buffer, SInt32 length);
-      void* ptRecv();
-      Boolean ptQuery();
-      static void ptInitQueue(SInt32 num_mod);
 };
 
 #endif

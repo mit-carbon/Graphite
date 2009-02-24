c58c55c7e830142f97a889745c65ec10e2d065d6
George Kurian <gkurian@mit.edu>
2009-02-23 21:10:18 -0500

    [jacobi] Modified the jacobi.c test a little
    Conflicts:
    
    	common/tests/1d_jacobi/jacobi.c

--- common/tests/1d_jacobi/jacobi.c
+++ common/tests/1d_jacobi/jacobi.c
@@ -4,43 +4,58 @@
 #include <stdlib.h>
 #include <unistd.h>
 #include <pthread.h>
+#include <assert.h>
+
 #include "capi.h"
 #include "sync_api.h"
 
-
 #define DEBUG 1
+// #define FULL_DEBUG 1
+// #define USE_INT 1
 
-typedef int Int32;
+#ifdef FULL_DEBUG
+#define DEBUG 1
+#endif
+
+typedef int SInt32;
 typedef unsigned int UInt32;
 
-#define NUM_ITERS 10
+#ifdef USE_INT
+typedef SInt32 DType;
+#else
+typedef float DType;
+#endif
+
+#define NUM_ITERS 500
 #define SWAP(a,b,t)  (((t) = (a)), ((a) = (b)), ((b) = (t)))
 
 // Global Variables
-Int32* g_old_array;
-Int32* g_new_array;
-Int32 g_num_cores;
-Int32 g_size;
+DType* g_old_array;
+DType* g_new_array;
+SInt32 g_num_cores;
+SInt32 g_size;
 
 carbon_barrier_t jacobi_barrier;
 
+#ifdef DEBUG
 pthread_mutex_t print_lock;
+#endif
 
 // Function Declarations
 void* threadMain(void*);
-void printArray(Int32);
+void printArray(SInt32);
 
-int wait_some()
+SInt32 wait_some()
 {
-   Int32 j = 0;
-   for (UInt32 i = 0; i < 20000; i++)
+   SInt32 j = 0;
+   for (UInt32 i = 0; i < 2000; i++)
    {
       j += i;
    }
    return j;
 }
 
-int main(int argc, char *argv[])
+SInt32 main(SInt32 argc, char *argv[])
 {
    if (argc != 3) {
       fprintf(stderr, "[Usage]: ./jacobi <Number_of_Cores> <Size_Multiplier>\n");
@@ -61,17 +76,20 @@ int main(int argc, char *argv[])
    fprintf (stderr, "Number of Cores = %d\n", g_num_cores);
    fprintf (stderr, "Size of the array = %d\n", g_size);
 
-   g_old_array = (Int32 *) malloc((g_size+2) * sizeof(Int32));
-   g_new_array = (Int32 *) malloc((g_size+2) * sizeof(Int32));
+   g_old_array = (DType *) malloc((g_size+2) * sizeof(DType));
+   g_new_array = (DType *) malloc((g_size+2) * sizeof(DType));
 
-   for (Int32 i = 0; i < g_size+2; i++) {
+   for (SInt32 i = 0; i < g_size+2; i++)
+   {
       g_old_array[i] = 0;
       g_new_array[i] = 0;
    }
    g_old_array[0] = 32768;
    g_new_array[0] = 32768;
-   
-   pthread_mutex_init (&print_lock, NULL);
+
+#ifdef DEBUG
+   pthread_mutex_init(&print_lock, NULL);
+#endif
 
    pthread_t threads[g_num_cores];
    pthread_attr_t attr;
@@ -85,29 +103,38 @@ int main(int argc, char *argv[])
    pthread_mutex_unlock(&print_lock);
 #endif
 
-   for (Int32 i = 0; i < g_num_cores; i++)
+   for (SInt32 i = 0; i < g_num_cores; i++)
    {
-#ifdef DEBUG
+#ifdef FULL_DEBUG
       pthread_mutex_lock(&print_lock);
       fprintf(stderr, "Creating Thread: %d\n", i);
       pthread_mutex_unlock(&print_lock);
 #endif
-      pthread_create(&threads[i], &attr, threadMain, (void*) i);
+
+      SInt32 ret = pthread_create(&threads[i], &attr, threadMain, (void*) i);
+      if (ret != 0) {
+         // fprintf(stderr, "ERROR spawning thread %d! Error code: %s\n",
+         //       i, strerror(ret));         
+         fprintf(stderr, "ERROR spawning thread %d!\n", i);         
+         exit(-1);
+      }
    }
 
-   for (Int32 i = 0; i < g_num_cores; i++)
+   for (SInt32 i = 0; i < g_num_cores; i++)
    {
       pthread_join(threads[i], NULL);
    }
+
+   printArray(NUM_ITERS);
 
    return (0);
 }
 
 void* threadMain(void *threadid)
 {
+   SInt32 tid = (SInt32) threadid;
 
-   Int32 tid = (Int32) threadid;
-
+   CarbonInitializeThread();
    CAPI_Initialize(tid);
 
    if (tid == 0)
@@ -115,6 +142,11 @@ void* threadMain(void *threadid)
 #ifdef DEBUG
       pthread_mutex_lock(&print_lock);
       fprintf(stderr, "Starting threadMain - Thread 0\n");
+      pthread_mutex_unlock(&print_lock);
+#endif
+
+#ifdef FULL_DEBUG
+      pthread_mutex_lock(&print_lock);
       printArray(-1);
       pthread_mutex_unlock(&print_lock);
 #endif
@@ -131,7 +163,7 @@ void* threadMain(void *threadid)
       wait_some();
    }
 
-#ifdef DEBUG
+#ifdef FULL_DEBUG
    pthread_mutex_lock(&print_lock);
    fprintf(stderr, "Thread [ %d ]: Waiting for barrier\n", tid);
    pthread_mutex_unlock(&print_lock);
@@ -145,19 +177,19 @@ void* threadMain(void *threadid)
    pthread_mutex_unlock(&print_lock);
 #endif
 
-   Int32 start_index = ((g_size/g_num_cores) * tid) + 1;
-   Int32 end_index = ((g_size/g_num_cores) * (tid + 1)) + 1;
+   SInt32 start_index = ((g_size/g_num_cores) * tid) + 1;
+   SInt32 end_index = ((g_size/g_num_cores) * (tid + 1)) + 1;
 
-#ifdef DEBUG
+#ifdef FULL_DEBUG
    pthread_mutex_lock(&print_lock);
    fprintf(stderr, "Thread [ %d ]: Start Index = %d\n", tid, start_index);
    fprintf(stderr, "Thread [ %d ]: End Index = %d\n", tid,  end_index);
    pthread_mutex_unlock(&print_lock);
 #endif
 
-   for (Int32 k = 0; k < NUM_ITERS; k++)
+   for (SInt32 k = 0; k < NUM_ITERS; k++)
    {
-      for (Int32 i = start_index; i < end_index; i++)
+      for (SInt32 i = start_index; i < end_index; i++)
       {
          g_new_array[i] = (g_old_array[i-1] + g_old_array[i+1]) / 2;
       }
@@ -166,10 +198,10 @@ void* threadMain(void *threadid)
 
       if (tid == 0)
       {
-         Int32* temp;
+         DType* temp;
          SWAP(g_old_array, g_new_array, temp);
 
-#ifdef DEBUG
+#ifdef FULL_DEBUG
          pthread_mutex_lock(&print_lock);
          printArray(k);
          pthread_mutex_unlock(&print_lock);
@@ -180,17 +212,29 @@ void* threadMain(void *threadid)
 
    }
 
-   pthread_exit(NULL);
+   return 0;
 
 }
 
-void printArray(Int32 iter)
+void printArray(SInt32 iter)
 {
 
    fprintf(stderr, "Contents of Array after iteration: %d\n", iter);
-   for (Int32 i = 0; i < g_size+2; i++)
+   for (SInt32 i = 0; i < g_size+2; i++)
    {
+#ifdef USE_INT
       fprintf(stderr, "%d, ", g_old_array[i]);
+#else
+      fprintf(stderr, "{ %f, ", g_old_array[i]);
+      UInt32 float_rep = *((UInt32*) &g_old_array[i]);
+      fprintf(stderr, "0x%x } , ", float_rep);
+      // float_rep = float_rep & 0x7fc00000;
+      if ( (float_rep & 0x7fc00000) == 0x7fc00000) {
+         fprintf(stderr, "\n\nEncountered a NaN : 0x%x !!!\n\n", float_rep);
+         assert (false);
+         exit(-1);
+      }
+#endif
    }
    fprintf(stderr, "\n\n");
 

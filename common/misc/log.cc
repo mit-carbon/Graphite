4101c8fc787033dcfad9e37b26acdb8aaaf11344
Nathan Beckmann <beckmann@mit.edu>
2009-03-06 02:28:22 -0500

    [log] Output base part of filename only.

--- common/misc/log.cc
+++ common/misc/log.cc
@@ -170,6 +170,11 @@ std::string Log::getModule(const char *filename)
       // build module string
       string mod;
 
+      // find actual file name ...
+      const char *ptr = strrchr(filename, '/');
+      if (ptr != NULL)
+         filename = ptr + 1;
+
       for (UInt32 i = 0; i < MODULE_LENGTH && filename[i] != '\0'; i++)
          mod.push_back(filename[i]);
 
@@ -188,6 +193,7 @@ std::string Log::getModule(const char *filename)
       return mod;
    }
 }
+
 void Log::log(ErrorState err, const char* source_file, SInt32 source_line, const char *format, ...)
 {
 #ifdef DISABLE_LOGGING

5e4f51b51d6c8fa4c8e3b1ec99073eda7c5dbfdc
Nathan Beckmann <beckmann@mit.edu>
2009-06-10 17:15:12 -0400

    [perf] Add simple memory model to consume latency of memory operations. Still needs them to be pushed.

--- common/performance_model/simple_performance_model.cc
+++ common/performance_model/simple_performance_model.cc
@@ -21,7 +21,33 @@ void SimplePerformanceModel::outputSummary(std::ostream &os)
 
 void SimplePerformanceModel::handleInstruction(Instruction *instruction)
 {
+   // compute cost
+   UInt64 cost = 0;
+
+   const OperandList &ops = instruction->getOperands();
+
+   for (unsigned int i = 0; i < ops.size(); i++)
+   {
+      const Operand &o = ops[i];
+
+      if (o.m_type == Operand::MEMORY &&
+          o.m_direction == Operand::READ)
+      {
+         DynamicInstructionInfo &i = getDynamicInstructionInfo();
+         LOG_ASSERT_ERROR(i.type == DynamicInstructionInfo::INFO_MEMORY,
+                          "Expected memory info.");
+
+         cost += i.memory_info.latency;
+         // ignore address
+         
+         popDynamicInstructionInfo();
+      }
+   }
+
+   cost += instruction->getCost();
+
+   // update counters
    m_instruction_count++;
-   m_cycle_count += instruction->getCost();
+   m_cycle_count += cost;
 }
 

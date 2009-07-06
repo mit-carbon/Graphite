bd08f047a30bf17d0817ac7dc21617359c1b908c
Nathan Beckmann <beckmann@mit.edu>
2009-07-05 20:05:11 -0400

    [tools] Change launch script to clean up processes and return error codes. Not thoroughly tested for multiprocess.

--- tools/launch.py
+++ tools/launch.py
@@ -2,6 +2,9 @@
 
 import sys
 import subprocess
+import time
+import signal
+import os
 
 start_proc = int(sys.argv[1])
 num_procs = int(sys.argv[2])
@@ -14,6 +17,7 @@ for p in sys.argv[3:len(sys.argv)]:
 
 procs = {}
 
+# start
 for i in range(start_proc, start_proc + num_procs):
     print "Starting process: %d" % i
     env = {}
@@ -22,5 +26,28 @@ for i in range(start_proc, start_proc + num_procs):
 
     procs[i] = subprocess.Popen(path, shell=True, env=env)
 
+# wait
+active = True
+returnCode = None
+
+while active:
+    time.sleep(0.1)
+    for i in range(start_proc, start_proc + num_procs):
+        returnCode = procs[i].poll()
+        if returnCode != None:
+            active = False
+            break
+
+# kill
 for i in range(start_proc, start_proc + num_procs):
-    procs[i].wait()
+    returnCode2 = procs[i].poll()
+    if returnCode2 == None:
+        os.kill(procs[i].pid, signal.SIGKILL)
+    elif returnCode == 0:
+        returnCode = returnCode2
+
+# exit
+if returnCode != None:
+    print >> sys.stderr, 'Exited with return code: %d' % returnCode
+
+sys.exit(returnCode)

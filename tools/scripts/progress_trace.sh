6b44409196d6e5db7bd2985c2058b203b7529879
Nathan Beckmann <beckmann@mit.edu>
2009-07-25 19:30:10 -0400

    [prog_trace] Generalize progress_trace.sh.

--- tools/scripts/progress_trace.sh
+++ tools/scripts/progress_trace.sh
@@ -1,1 +1,1 @@
-for i in $(find . -name ARG\*) ; do echo $i ; cd $i && python ~/research/sim/tools/scripts/progress_trace.py > data.m && math < ~/research/sim/tools/scripts/progress_trace.m && cd .. ; done
+for i in $(find . -type d -not -name .) ; do echo $i ; cd $i && python ${GRAPHITE_ROOT}/tools/scripts/progress_trace.py > data.m && math < ${GRAPHITE_ROOT}/tools/scripts/progress_trace.m && cd .. ; done

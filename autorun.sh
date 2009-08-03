63b9ffc1e95e14981d4333bdc0ede2a16daddc37
George Kurian <gkurian@mit.edu>
2009-08-03 13:22:42 -0400

    [tests] Removed the scipt for compliling the simulator with '-O3' and app with '-O0'

--- autorun.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-sed -i s/-O0/-O3/ common/Makefile.common
-make clean && make
-mv lib/pin_sim.so temp/
-sed -i s/-O3/-O0/ common/Makefile.common
-make clean && make
-mv temp/pin_sim.so lib/

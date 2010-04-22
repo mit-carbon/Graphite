cc3e6c6482a88456979ed03b26b1045dc342a11e
George Kurian <gkurian@mit.edu>
2010-04-22 14:17:52 -0400

    [shared mem] Removed the MOSI cache coherence protocol

--- common/core/memory_subsystem/memory_manager_base.h
+++ common/core/memory_subsystem/memory_manager_base.h
@@ -16,7 +16,6 @@ class MemoryManagerBase
       enum CachingProtocol_t
       {
          PR_L1_PR_L2_DRAM_DIRECTORY_MSI = 0,
-         PR_L1_PR_L2_DRAM_DIRECTORY_MOSI,
          NUM_CACHING_PROTOCOL_TYPES
       };
 

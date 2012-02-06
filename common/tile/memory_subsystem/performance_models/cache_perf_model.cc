d61b5efe9c01ed81489fa9e351cef2ee8d6cfd65
George Kurian <gkurian@mit.edu>
2012-02-06 18:55:29 -0500

    [cache performance models] Renovated the cache performance models

--- common/tile/memory_subsystem/performance_models/cache_perf_model.cc
+++ common/tile/memory_subsystem/performance_models/cache_perf_model.cc
@@ -6,28 +6,27 @@
 #include "cache_perf_model_sequential.h"
 #include "log.h"
 
-CachePerfModel::CachePerfModel(UInt64 cache_data_access_delay_in_clock_cycles, UInt64 cache_tags_access_delay_in_clock_cycles, volatile float core_frequency):
-   m_cache_data_access_delay_in_clock_cycles(cache_data_access_delay_in_clock_cycles),
-   m_cache_tags_access_delay_in_clock_cycles(cache_tags_access_delay_in_clock_cycles)
-{
-}
+CachePerfModel::CachePerfModel(UInt64 data_array_access_time, UInt64 tag_array_access_time, float core_frequency)
+   : _data_array_access_time(data_array_access_time)
+   , _tag_array_access_time(tag_array_access_time)
+{}
 
 CachePerfModel::~CachePerfModel()
 {}
 
 CachePerfModel* 
-CachePerfModel::create(std::string cache_perf_model_type,
-      UInt64 cache_data_access_delay_in_clock_cycles, UInt64 cache_tags_access_delay_in_clock_cycles, float core_frequency)
+CachePerfModel::create(std::string cache_perf_model_type, UInt64 data_array_access_time,
+                       UInt64 tag_array_access_time, float core_frequency)
 {
    PerfModel_t perf_model = parseModelType(cache_perf_model_type);
 
    switch(perf_model)
    {
       case(CACHE_PERF_MODEL_PARALLEL):
-         return new CachePerfModelParallel(cache_data_access_delay_in_clock_cycles, cache_tags_access_delay_in_clock_cycles, core_frequency);
+         return new CachePerfModelParallel(data_array_access_time, tag_array_access_time, core_frequency);
             
       case(CACHE_PERF_MODEL_SEQUENTIAL):
-         return new CachePerfModelSequential(cache_data_access_delay_in_clock_cycles, cache_tags_access_delay_in_clock_cycles, core_frequency);
+         return new CachePerfModelSequential(data_array_access_time, tag_array_access_time, core_frequency);
             
       default:
          LOG_ASSERT_ERROR(false, "Unsupported CachePerfModel type: %s", cache_perf_model_type.c_str());
@@ -39,16 +38,10 @@ CachePerfModel::PerfModel_t
 CachePerfModel::parseModelType(std::string model_type)
 {
    if (model_type == "parallel")
-   {
       return CACHE_PERF_MODEL_PARALLEL;
-   }
    else if (model_type == "sequential")
-   {
       return CACHE_PERF_MODEL_SEQUENTIAL;
-   }
    else 
-   {
       LOG_PRINT_ERROR("Unsupported CacheModel type: %s", model_type.c_str());
-      return NUM_CACHE_PERF_MODELS;
-   }
+   return NUM_CACHE_PERF_MODELS;
 }

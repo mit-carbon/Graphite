10638c51a097bdd4452cbc65b6c9d1be6fbe3315
George Kurian <gkurian@mit.edu>
2012-07-21 23:02:47 -0400

    [automated tests] Enabled keyboard interrupt + colors

--- tools/tests.py
+++ tools/tests.py
@@ -8,59 +8,49 @@ sys.path.append("./tools/")
 from schedule import *
 
 # job info
+# Do not use 'localhost' or '127.0.0.1', use the machine name
 machines = [
-    "cagnode1",
-    "cagnode1",
-    "cagnode2",
-    "cagnode2",
-    "cagnode3",
-    "cagnode3",
-    "cagnode4",
-    "cagnode4",
-    "cagnode8",
-    "cagnode8",
-    "cagnode9",
-    "cagnode9"
+    "cagnode5",
+    "cagnode14",
+    "cagnode15"
     ]
 
-results_dir = "./results/64-core"
-cfg_file = "carbon_sim_64.cfg"
+results_dir = "./results/test"
+cfg_file = "carbon_sim.cfg"
 
-benchmarks = ["fft", "radix", "barnes", "ocean_contiguous", "fmm", "lu_contiguous"]
+benchmarks = ["fft", "radix", "lu_contiguous", "lu_non_contiguous", "cholesky", "barnes", "fmm", "ocean_contiguous", "ocean_non_contiguous", "water-nsquared", "water-spatial", "raytrace", "volrend"]
 commands = ["./tests/benchmarks/fft/fft -p64 -m16",
             "./tests/benchmarks/radix/radix -p64",
-            "./tests/benchmarks/barnes/barnes \< ./tests/benchmarks/barnes/input",
+            "./tests/benchmarks/lu_contiguous/lu_contiguous -p64",
+            "./tests/benchmarks/lu_non_contiguous/lu_non_contiguous -p64 -n512",
+            "./tests/benchmarks/cholesky/cholesky -p64 ./tests/benchmarks/cholesky/inputs/tk15.O",
+            "./tests/benchmarks/barnes/barnes < ./tests/benchmarks/barnes/input",
+            "./tests/benchmarks/fmm/fmm < ./tests/benchmarks/fmm/inputs/input.16384",
             "./tests/benchmarks/ocean_contiguous/ocean_contiguous -p64",
-            "./tests/benchmarks/fmm/fmm \< ./tests/benchmarks/fmm/inputs/input.65536",
-            "./tests/benchmarks/lu_contiguous/lu_contiguous -p64"]
-protocols = ["ackwise","full_map","limited_no_broadcast"]
-networks = ["emesh","atac"]
-hardware_sharer_count = 4
+            "./tests/benchmarks/ocean_non_contiguous/ocean_non_contiguous -p64",
+            "./tests/benchmarks/water-nsquared/water-nsquared < ./tests/benchmarks/water-nsquared/input",
+            "./tests/benchmarks/water-spatial/water-spatial < ./tests/benchmarks/water-spatial/input",
+            "./tests/benchmarks/raytrace/raytrace -p64 -m64 ./tests/benchmarks/raytrace/inputs/car.env",
+            "./tests/benchmarks/volrend/volrend 64 ./tests/benchmarks/volrend/inputs/head"]
 
 jobs = []
 
-jobs.append(LocalJob(12, "echo Starting"))
-
 for benchmark, command in zip(benchmarks, commands):
-   for protocol in protocols:
-      for network in networks:
-         sim_flags = "-c %s/%s --general/total_cores=64 --general/enable_shared_mem=true --network/memory_model_1=finite_buffer_%s --perf_model/dram_directory/directory_type=%s --perf_model/dram_directory/max_hw_sharers=%i" % (results_dir, cfg_file, network, protocol, hardware_sharer_count)
-         sub_dir = "%s-%s-%s" % (benchmark, protocol, network)
-         jobs.append(MakeJob(1, command, results_dir, sub_dir, sim_flags, "pin"))
-
-jobs.append(LocalJob(12, "echo Finished"))
+   make_cmd = "make -C tests/benchmarks/%s clean && make %s_bench_test BUILD_MODE=build" % (benchmark, benchmark)
+   os.system(make_cmd)
+   
+   sim_flags = "-c %s/%s --general/total_cores=64 --general/enable_shared_mem=true" % (results_dir, cfg_file)
+   sub_dir = "%s" % (benchmark)
+   print sim_flags
+   print sub_dir
+   jobs.append(MakeJob(1, command, results_dir, sub_dir, sim_flags, "pin"))
 
 # jobs = [
-#     LocalJob(1, "echo Starting..."),
 #     MakeJob(1, command, results_dir, "1", sim_flags),
 #     MakeJob(1, command, results_dir, "2", sim_flags),
 #     MakeJob(1, command, results_dir, "3", sim_flags),
 #     MakeJob(1, command, results_dir, "4", sim_flags),
-#     LocalJob(1, "echo Finished."),
 #     ]
-# 
-# uncomment this line to kill all the fmms
-#jobs = [ SpawnJob(12, "killall -9 fmm") ]
 
 # init
 try:

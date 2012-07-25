d0756adb0d6b71c8982d22047f104423b5604254
Anthony J Giovinazzo <tony_g@cagnode16.csail.mit.edu>
2012-07-25 13:17:14 -0400

    Removed hard coding of LD_LIBRARY_PATH.

--- tools/spawn.py
+++ tools/spawn.py
@@ -8,14 +8,12 @@ import sys
 import os
 import subprocess
 
-l_path = "/afs/csail/group/carbon/tools/pin/pintest/intel64/runtime"
 
 # spawn_job:
 #  start up a command on one machine
 #  can be called by spawn_master.py or spawn.py
 def spawn_job(proc_num, command):
    os.environ['CARBON_PROCESS_INDEX'] = "%d" % (proc_num)
-   os.environ['LD_LIBRARY_PATH'] = l_path
 
    proc = subprocess.Popen(command, shell=True, preexec_fn=os.setsid, env=os.environ)
    return proc

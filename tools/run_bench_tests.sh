8c153220c700a2902923125444b499e5dd3d8e4f
George Kurian <gkurian@mit.edu>
2012-10-27 14:11:41 -0400

    [tools] Changed tools/run_bench_tests.sh to a more compatible format for dash,bash,csh

--- tools/run_bench_tests.sh
+++ tools/run_bench_tests.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 date
-make regress_bench >&run_bench_tests.log
+make regress_bench >run_bench_tests.log 2>&1
 date
 grep "^TEST:" run_bench_tests.log >run_bench_tests.tmp
 cat run_bench_tests.tmp >>run_bench_tests.log

4e184649492b9ec422d0de3138a0ff8d1e321b64
George Kurian <gkurian@mit.edu>
2012-11-25 22:54:48 -0500

    [regress] Changes to infrastructure

--- tools/regress/aggregate_results.py
+++ tools/regress/aggregate_results.py
@@ -34,16 +34,18 @@ def geomean(num_list):
 
 # Print a summary of the regression tests
 summary_file = open("./tools/regress/summary.log", 'w')
-host_time_list = []
-KIPS_list = []
+host_time_list_1 = []
+KIPS_list_1 = []
+host_time_list_2 = []
+KIPS_list_2 = []
 
 summary_file.write("_" * 100)
 summary_file.write("\n\n")
-summary_file.write("%s | %s | %s |  %s |  %s |\n" % ('Benchmarks'.center(23), '# Machines'.center(12), 'Status'.center(8), 'Simulated Time'.center(16), 'Performance (in KIPS)'.center(25)))
+summary_file.write("%s | %s | %s |  %s |  %s |\n" % ('Benchmarks'.center(23), '# Machines'.center(12), 'Status'.center(8), 'Simulation Time'.center(16), 'Performance (in KIPS)'.center(25)))
 summary_file.write("_" * 100)
 summary_file.write("\n\n")
-for benchmark in benchmarks:
-   for num_machines in num_machines_list:
+for num_machines in num_machines_list:
+   for benchmark in benchmarks:
       cmd = "\n./tools/parse_output.py --input-file %s/%s/sim.out --stats-file %s/%s/stats.out --num-cores 64" \
             % (results_dir, benchmark, results_dir, benchmark)
       print cmd
@@ -59,12 +61,17 @@ for benchmark in benchmarks:
          formatted_KIPS = "%.2f" % (KIPS)
          summary_file.write("%s | %s | %s |  %s |  %s |\n" % (benchmark.ljust(23), str(num_machines).center(12), 'PASS'.center(8), formatTime(host_time).ljust(16), formatted_KIPS.ljust(25)))
          
-         host_time_list.append(host_time)
-         KIPS_list.append(KIPS)
+         if (num_machines == 1):
+            host_time_list_1.append(host_time)
+            KIPS_list_1.append(KIPS)
+         else:
+            host_time_list_2.append(host_time)
+            KIPS_list_2.append(KIPS)
       else:
          summary_file.write("%s | %s | %s |  %s |  %s |\n" % (benchmark.ljust(23), str(num_machines).center(12), 'FAIL'.center(8), ''.ljust(16), ''.ljust(25)))
 summary_file.write("_" * 100)
-summary_file.write("\n")
+summary_file.write("\n\n")
 
-summary_file.write("\n%s (total), %.2f KIPS (geomean)\n" % (formatTime(sum(host_time_list)), geomean(KIPS_list)))
+summary_file.write("1 Machine  : Simulation Time - %s (total), Performance - %.2f KIPS (geomean)\n" % (formatTime(sum(host_time_list_1)), geomean(KIPS_list_1)))
+summary_file.write("2 Machines : Simulation Time - %s (total), Performance - %.2f KIPS (geomean)\n" % (formatTime(sum(host_time_list_2)), geomean(KIPS_list_2)))
 summary_file.close()

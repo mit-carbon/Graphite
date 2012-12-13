e2f510c6c1217643b5926fd5097958e384dc1040
George Kurian <gkurian@mit.edu>
2012-12-13 05:00:48 -0500

    [tests][open_mp] Flush output

--- tests/apps/open_mp/open_mp.cc
+++ tests/apps/open_mp/open_mp.cc
@@ -7,17 +7,17 @@ int main( int argc, char *argv[])
 {
    int nthreads, tid;
 
-   omp_set_num_threads(2);
-
    #pragma omp parallel private(tid,nthreads) num_threads(2)
    {
       tid = omp_get_thread_num();
       printf("Hello World from thread %d\n", tid);
+      fflush(NULL);
 
       if (tid == 0)
       {
          nthreads = omp_get_num_threads();
          printf("Number of threads %d\n", nthreads);
+         fflush(NULL);
       }
    }
 

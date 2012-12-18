f27a3e49e7c03905982443959d609b58dffbd88b
Anthony J Giovinazzo <tony_g@mad10.csail.mit.edu>
2012-12-18 12:21:33 -0500

    Added syscalls fcntl and getdents

--- pin/handle_syscalls.cc
+++ pin/handle_syscalls.cc
@@ -112,6 +112,8 @@ void syscallEnterRunModel(THREADID threadIndex, CONTEXT *ctx, SYSCALL_STANDARD s
             (syscall_number == SYS_access) ||
             (syscall_number == SYS_flock) ||
             (syscall_number == SYS_fchmod) ||
+            (syscall_number == SYS_fcntl) ||
+            (syscall_number == SYS_getdents) ||
             (syscall_number == SYS_rmdir) ||
             (syscall_number == SYS_unlink) ||
             (syscall_number == SYS_clock_gettime) ||
@@ -252,6 +254,8 @@ void syscallExitRunModel(THREADID threadIndex, CONTEXT *ctx, SYSCALL_STANDARD sy
             (syscall_number == SYS_access) ||
             (syscall_number == SYS_flock) ||
             (syscall_number == SYS_fchmod) ||
+            (syscall_number == SYS_fcntl) ||
+            (syscall_number == SYS_getdents) ||
             (syscall_number == SYS_rmdir) ||
             (syscall_number == SYS_unlink) ||
             (syscall_number == SYS_clock_gettime) ||

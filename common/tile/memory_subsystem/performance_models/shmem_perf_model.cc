06fbdda55d29b4e9418a099ad1828433e5c9a700
George B. P. Bezerra <gbezerra@csail.mit.edu>
2013-02-08 09:56:11 -0500

    [absolute time] - Converted the ShmemPerfModel to absolute time.

--- common/tile/memory_subsystem/performance_models/shmem_perf_model.cc
+++ common/tile/memory_subsystem/performance_models/shmem_perf_model.cc
@@ -1,38 +1,81 @@
 #include "shmem_perf_model.h"
 #include "log.h"
+#include "simulator.h"
+#include "tile_manager.h"
+#include "tile.h"
 
 ShmemPerfModel::ShmemPerfModel()
-   : _cycle_count(0)
+   : _curr_time(0)
    , _enabled(false)
 {}
 
+
 ShmemPerfModel::~ShmemPerfModel()
 {}
+
+
+void 
+ShmemPerfModel::setCurrTime(Time& time)
+{
+   LOG_PRINT("setCurrTime: count(%llu)", time._picosec);
+   _curr_time = time;
+}
+
+Time
+ShmemPerfModel::getCurrTime()
+{
+   return _curr_time;
+}
+
+void
+ShmemPerfModel::updateCurrTime(Time& time)
+{
+   LOG_PRINT("updateCurrTime: time(%llu)", time._picosec);
+   if (_curr_time < time)
+      _curr_time = time;
+}
+
+void
+ShmemPerfModel::incrCurrTime(Time& time)
+{
+   if (_enabled)
+      _curr_time = _curr_time + time;
+}
+
+
+
 
 void 
 ShmemPerfModel::setCycleCount(UInt64 count)
 {
    LOG_PRINT("setCycleCount: count(%llu)", count);
-   _cycle_count = count;
+   Latency lat(count, Sim()->getTileManager()->getCurrentTile()->getFrequency());
+   Time time(lat.toPicosec());
+   setCurrTime(time);
 }
 
 UInt64
 ShmemPerfModel::getCycleCount()
 {
-   return _cycle_count;
+
+   return _curr_time.toCycles(Sim()->getTileManager()->getCurrentTile()->getFrequency());
 }
 
 void
 ShmemPerfModel::updateCycleCount(UInt64 cycle_count)
 {
    LOG_PRINT("updateCycleCount: cycle_count(%llu)", cycle_count);
-   if (_cycle_count < cycle_count)
-      _cycle_count = cycle_count;
+   Latency lat(cycle_count, Sim()->getTileManager()->getCurrentTile()->getFrequency());
+   Time time(lat.toPicosec());
+   updateCurrTime(time);
 }
 
 void
 ShmemPerfModel::incrCycleCount(UInt64 count)
 {
-   if (_enabled)
-      _cycle_count += count;
+   Latency lat(count, Sim()->getTileManager()->getCurrentTile()->getFrequency());
+   Time time(lat.toPicosec());
+   incrCurrTime(time);
 }
+
+
--- common/tile/memory_subsystem/performance_models/shmem_perf_model.h
+++ common/tile/memory_subsystem/performance_models/shmem_perf_model.h
@@ -1,6 +1,7 @@
 #pragma once
 
 #include "fixed_types.h"
+#include "time_types.h"
 
 class ShmemPerfModel
 {
@@ -8,15 +9,21 @@ public:
    ShmemPerfModel();
    ~ShmemPerfModel();
 
+   void setCurrTime(Time& time);
+   Time getCurrTime();
+   void incrCurrTime(Time& time);
+   void updateCurrTime(Time& time);
+
    void setCycleCount(UInt64 count);
    UInt64 getCycleCount();
    void incrCycleCount(UInt64 count);
    void updateCycleCount(UInt64 count);
-   
+
    void enable()     { _enabled = true;  }
    void disable()    { _enabled = false; }
 
 private:
+   Time _curr_time;
    UInt64 _cycle_count;
    bool _enabled;
 };

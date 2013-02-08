4fa04ef0ff57b84f87688bbb3c3739ec3086455b
George B. P. Bezerra <gbezerra@csail.mit.edu>
2013-02-08 09:14:07 -0500

    [absolute time] - Created Latency and Time classes in time_types.h

---
+++ common/misc/time_types.h
@@ -0,0 +1,77 @@
+#ifndef _TIME_TYPES_H_
+#define _TIME_TYPES_H_
+
+#include <fixed_types.h>
+#include <log.h>
+#include <cmath>
+
+typedef volatile float Frequency;
+
+
+class Latency
+{
+   public:
+      Latency(UInt64 cycles, Frequency freq):_cycles(cycles), _freq(freq){};
+      Latency(const Latency& lat):_cycles(lat._cycles){};
+      ~Latency(){};
+
+      Latency operator+(const Latency& lat);
+
+      UInt64 toPicosec() const;
+
+      UInt64 _cycles;
+
+   private:
+      Frequency _freq;
+
+};
+
+class Time
+{
+   public:
+      Time(UInt64 picosec):_picosec(picosec){};
+      Time(const Time& time):_picosec(time._picosec){};
+      ~Time(){};
+
+      Time operator+(const Time& time){return Time(_picosec + time._picosec); };
+      Time operator+(const Latency& lat){return Time(_picosec + lat.toPicosec()); };
+      bool operator>(const Time& time){return _picosec > time._picosec; };
+      bool operator<(const Time& time){return _picosec < time._picosec; };
+
+      UInt64 toCycles(Frequency freq);
+
+      UInt64 _picosec;
+
+};
+
+
+inline UInt64 Latency::toPicosec() const
+{
+   UInt64 picosec = (UInt64) ceil( ((double) 1000*_cycles) /  ((double) _freq) );
+
+   LOG_PRINT("Convert cycles(%llu) with frequency(%f) to picoseconds(%llu)",
+             _cycles, _freq, _freq, picosec);
+
+   return picosec;
+}
+
+inline Latency Latency::operator+(const Latency& lat)
+{
+   LOG_ASSERT_ERROR(_freq == lat._freq,
+      "Attempting to add latencies from different frequencies");
+
+   return Latency(_cycles + lat._cycles, _freq);
+}
+
+inline UInt64 Time::toCycles(Frequency freq) 
+{
+   UInt64 cycles = (UInt64) ceil(((double) (_picosec) * ((double) freq))/1000.0);
+
+   LOG_PRINT("Convert picoseconds(%llu) with frequency(%f) to cycles(%llu)",
+             _picosec, freq, cycles);
+
+   return cycles;
+}
+
+
+#endif

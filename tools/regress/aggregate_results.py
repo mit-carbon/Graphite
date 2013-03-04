b7f0efabc07bb3a7f1d4d3c23cf21926a95509ac
George Kurian <gkurian@mit.edu>
2013-03-03 23:39:33 -0500

    [regress] Changed regression infrastructure to run both SPLASH-2 and PARSEC tests. Benchmarks that work in lite mode run in lite mode only.

--- tools/regress/aggregate_results.py
+++ tools/regress/aggregate_results.py
@@ -44,15 +44,21 @@ summary_file.write("\n\n")
 summary_file.write("%s | %s | %s |  %s |  %s |\n" % ('Benchmarks'.center(23), '# Machines'.center(12), 'Status'.center(8), 'Simulation Time'.center(16), 'Performance (in KIPS)'.center(25)))
 summary_file.write("_" * 100)
 summary_file.write("\n\n")
-for num_machines in num_machines_list:
-   for benchmark in benchmark_list:
-      cmd = "python -u ./tools/parse_output.py --input-file %s/%s-%i/sim.out --stats-file %s/%s-%i/stats.out --num-cores 64" \
+
+for benchmark in benchmark_list:
+   for num_machines in num_machines_list:
+      
+      # Don't aggregate results for benchmarks that work only in lite mode on multiple machines
+      if (benchmark in lite_mode_list) and (num_machines > 1):
+         continue
+
+      cmd = "python -u ./tools/parse_output.py --input-file %s/%s--procs-%i/sim.out --stats-file %s/%s--procs-%i/stats.out --num-cores 64" \
             % (results_dir, benchmark, num_machines, results_dir, benchmark, num_machines)
       print cmd
       ret = os.system(cmd)
 
       if (ret == 0):
-         eventCounterInfo = open("%s/%s-%i/stats.out" % (results_dir, benchmark, num_machines), 'r').readlines()
+         eventCounterInfo = open("%s/%s--procs-%i/stats.out" % (results_dir, benchmark, num_machines), 'r').readlines()
 
          total_target_instructions = parseEventCounters("Target-Instructions", eventCounterInfo)
          host_time = parseEventCounters("Host-Time", eventCounterInfo)                                # In micro-seconds
@@ -64,7 +70,7 @@ for num_machines in num_machines_list:
          if (num_machines == 1):
             host_time_list_1.append(host_time)
             KIPS_list_1.append(KIPS)
-         else:
+         else: # num_machines == 2
             host_time_list_2.append(host_time)
             KIPS_list_2.append(KIPS)
       else:

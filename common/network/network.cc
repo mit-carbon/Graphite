8510a285ee6558b2cc2dd270cf9d081053f10ccf
George Kurian <gkurian@mit.edu>
2013-03-08 14:21:29 -0500

    [network][sim.out] Do not print out the freq_control network stats

--- common/network/network.cc
+++ common/network/network.cc
@@ -83,6 +83,8 @@ void Network::outputSummary(std::ostream &out) const
    out << "Network Summary: " << endl;
    for (UInt32 i = 0; i < NUM_STATIC_NETWORKS; i++)
    {
+      if (i > STATIC_NETWORK_SYSTEM)
+         break;         
       out << "  Network - " <<  _models[i]->getNetworkName() << ": " << endl;
       _models[i]->outputSummary(out);
    }

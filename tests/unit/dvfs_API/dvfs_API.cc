dbcf972932de4c24e1de2fa71188710214f2de32
George Kurian <gkurian@mit.edu>
2013-04-23 11:58:11 -0400

    [tests][dvfs] Corrected the dvfs unit test

--- tests/unit/dvfs_API/dvfs_API.cc
+++ tests/unit/dvfs_API/dvfs_API.cc
@@ -24,24 +24,22 @@ int main()
       exit(EXIT_FAILURE);
    }
 
-   // invalid module type
+   // invalid frequency
    rc = CarbonSetDVFS(0, 0x2f, &frequency, AUTO);
    printf("test 3) Voltage: %g, Frequency: %g, RC: %i\n", voltage, frequency, rc);
-   if (rc != -2){
-      fprintf(stderr, "Expected RC = -2, got RC = %i", rc) ;
+   if (rc != -4){
+      fprintf(stderr, "Expected RC = -4, got RC = %i", rc) ;
       exit(EXIT_FAILURE);
    }
 
-   // invalid module type
-   /*
-   frequency = 1.0;
-   rc = CarbonSetDVFS(0, L2_DIRECTORY, &frequency, AUTO);
-   printf("test 4) Voltage: %g, Frequency: %g, RC: %i\n", voltage, frequency, rc);
-   if (rc != -2){
-      fprintf(stderr, "Expected RC = -2, got RC = %i", rc) ;
-      exit(EXIT_FAILURE);
-   }
-*/
+   // invalid module type (works only for pr_l1_sh_l2_msi model)
+   // frequency = 1.0;
+   // rc = CarbonSetDVFS(0, L2_DIRECTORY, &frequency, AUTO);
+   // printf("test 4) Voltage: %g, Frequency: %g, RC: %i\n", voltage, frequency, rc);
+   // if (rc != -2){
+   //    fprintf(stderr, "Expected RC = -2, got RC = %i", rc) ;
+   //    exit(EXIT_FAILURE);
+   // }
 
    // invalid voltage option
    rc = CarbonSetDVFS(0, TILE, &frequency, (voltage_option_t) 5);

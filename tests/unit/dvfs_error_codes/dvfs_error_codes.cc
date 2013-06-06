7140d112328f157297d2451b9b2c30807c3c255d
George B. P. Bezerra <gbezerra@csail.mit.edu>
2013-06-06 08:14:13 -0400

    [dvfs] Changed unit test to work with new DVFS interface implementation.

--- tests/unit/dvfs_error_codes/dvfs_error_codes.cc
+++ tests/unit/dvfs_error_codes/dvfs_error_codes.cc
@@ -8,27 +8,29 @@ int main()
    double voltage = 0;
    int rc;
 
+   module_t domain = (module_t)CarbonGetDVFSDomain(CORE);
+
    // invalid tile id
-   rc = CarbonGetDVFS(-1, CORE, &frequency, &voltage);
+   rc = CarbonGetDVFS(-1, domain, &frequency, &voltage);
    printf("test 1) Voltage: %g, Frequency: %g, RC: %i\n", voltage, frequency, rc);
    if (rc != -1){
-      fprintf(stderr, "Expected RC = -1, got RC = %i", rc) ;
+      fprintf(stderr, "Expected RC = -1, got RC = %i\n", rc) ;
       exit(EXIT_FAILURE);
    }
 
-   // invalid module type
-   rc = CarbonGetDVFS(0, TILE, &frequency, &voltage);
+   // invalid domain 
+   rc = CarbonGetDVFS(0, CORE, &frequency, &voltage);
    printf("test 2) Voltage: %g, Frequency: %g, RC: %i\n", voltage, frequency, rc);
    if (rc != -2){
-      fprintf(stderr, "Expected RC = -2, got RC = %i", rc) ;
+      fprintf(stderr, "Expected RC = -2, got RC = %i\n", rc) ;
       exit(EXIT_FAILURE);
    }
 
    // invalid frequency
-   rc = CarbonSetDVFS(0, 0xf, &frequency, AUTO);
+   rc = CarbonSetDVFSAllTiles(domain, &frequency, AUTO);
    printf("test 3) Voltage: %g, Frequency: %g, RC: %i\n", voltage, frequency, rc);
    if (rc != -4){
-      fprintf(stderr, "Expected RC = -4, got RC = %i", rc) ;
+      fprintf(stderr, "Expected RC = -4, got RC = %i\n", rc) ;
       exit(EXIT_FAILURE);
    }
 
@@ -43,30 +45,30 @@ int main()
    }*/
 
    // invalid voltage option
-   rc = CarbonSetDVFS(0, TILE, &frequency, (voltage_option_t) 5);
+   rc = CarbonSetDVFSAllTiles(domain, &frequency, (voltage_option_t) 5);
    printf("test 5) Voltage: %g, Frequency: %g, RC: %i\n", voltage, frequency, rc);
    if (rc != -3){
-      fprintf(stderr, "Expected RC = -3, got RC = %i", rc) ;
+      fprintf(stderr, "Expected RC = -3, got RC = %i\n", rc) ;
       exit(EXIT_FAILURE);
    }
 
    // invalid frequency 
    frequency = 100;
-   rc = CarbonSetDVFS(0, TILE, &frequency, AUTO);
+   rc = CarbonSetDVFSAllTiles(domain, &frequency, AUTO);
    printf("test 6) Voltage: %g, Frequency: %g, RC: %i\n", voltage, frequency, rc);
    if (rc != -4){
-      fprintf(stderr, "Expected RC = -4, got RC = %i", rc) ;
+      fprintf(stderr, "Expected RC = -4, got RC = %i\n", rc) ;
       exit(EXIT_FAILURE);
    }
 
    // above max frequency for current voltage
    frequency = 0.1;
-   rc = CarbonSetDVFS(0, TILE, &frequency, AUTO);
+   rc = CarbonSetDVFSAllTiles(domain, &frequency, AUTO);
    frequency = 2.0;
-   rc = CarbonSetDVFS(0, TILE, &frequency, HOLD);
+   rc = CarbonSetDVFSAllTiles(domain, &frequency, HOLD);
    printf("test 7) Voltage: %g, Frequency: %g, RC: %i\n", voltage, frequency, rc);
    if (rc != -5){
-      fprintf(stderr, "Expected RC = -5, got RC = %i", rc) ;
+      fprintf(stderr, "Expected RC = -5, got RC = %i\n", rc) ;
       exit(EXIT_FAILURE);
    }
    
